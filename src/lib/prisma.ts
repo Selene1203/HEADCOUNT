6801fc1ec0b6b9d33be19de6e005ef463b710e00
boitumelorakoloi-lab <boiturakoloi@gmail.com>
2026-04-05 12:06:17 +0200

    Delete src directory

--- src/lib/prisma.ts
+++
@@ -1,8 +0,0 @@
-// src/lib/prisma.ts
-// Single shared Prisma client instance used across all route files
-
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export default prisma;
